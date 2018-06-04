0c1b83104ee31fa20a6acf53c2049ccdbd343901
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-06-04 21:01:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,16 +5,15 @@ class Song
   extend Memorable::ClassMethods
   extend Findable
   include Paramable
-  include Memborable::IstanceMethods
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  #def initialize
-    #@@songs << self
-  #end
+  def initialize
+    @@songs << self
+  end
 
   #def self.find_by_name(name)
     #@@songs.detect{|a| a.name == name}
